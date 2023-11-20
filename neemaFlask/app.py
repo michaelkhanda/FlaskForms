22520446340d5e76104179ec94a79ab3e72c4af0
Michael Khanda <michaelkhanda006@gmail.com>
2023-11-21 01:45:59 +0300

    updated commit

---
+++ neemaFlask/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+
+class SponsorForm(FlaskForm):
+    sponsor_name = StringField('Sponsor Name', validators=[DataRequired()])
+    donation_type = SelectField('Donation Type',
+                                choices=[('money', 'Donated Money'), ('resources', 'Donated Resources')],
+                                validators=[DataRequired()])
+    donation_details = TextAreaField('Donation Details', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/add_sponsor', methods=['GET', 'POST'])
+def add_sponsor():
+    form = SponsorForm()
+
+    if form.validate_on_submit():
+        # Process the form data (e.g., save to a database)
+        sponsor_name = form.sponsor_name.data
+        donation_type = form.donation_type.data
+        donation_details = form.donation_details.data
+
+        # Here you can handle the data as needed (e.g., save to a database)
+
+        return f'Sponsor added: {sponsor_name}, Donation Type: {donation_type}, Details: {donation_details}'
+
+    return render_template('add_sponsor.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
